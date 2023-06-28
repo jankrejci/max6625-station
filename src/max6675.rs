c891468661af9c13a30bbfc5936aad2ba1d3c122
Jan Krejci <krejci.cze@gmail.com>
2023-06-28 09:30:13 +0200

    Add errors to cover situation when the sensor connection is bad

--- src/max6675.rs
+++ src/max6675.rs
@@ -32,14 +32,24 @@ impl MAX6675 {
 
         let mut value: usize = (value[0] as usize) << 8 | value[1] as usize;
 
+        // If the sensor is mountend in wrong way, it can hold MISO high.
+        if value == 0xFFFF_FFFF {
+            return Err(anyhow!(
+                "Sensor connection is probaly wrong, value 0x{value:08X}"
+            ));
+        }
         // If there are no data on the bus, there are still SCK present,
         // therefore 0x0 is read.
-        if value == 0 {
-            return Err(anyhow!("Sensor is not present"));
+        if value == 0x0000_0000 {
+            return Err(anyhow!(
+                "Sensor is probaly not present, value 0x{value:08X}"
+            ));
         }
-
+        // Bit D2 indicates should be normally low.
         if value & 0x4 == 0x4 {
-            return Err(anyhow!("Temerature is NaN"));
+            return Err(anyhow!(
+                "Thermocouple is probably not connected, value 0x{value:08X}"
+            ));
         }
 
         // First 3 bits are just status flags
