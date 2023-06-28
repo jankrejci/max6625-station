32cef74e7bd780d19961d46f903d783a9cacbb25
Jan Krejčí <jan.krejci@braiins.cz>
2023-06-28 14:32:42 +0200

    Load calibration offsets from file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 mod max6675;
 mod spi;
 
+use anyhow::{Context, Result};
 use max6675::MAX6675;
 use rocket::State;
 use spi::Spi;
 use std::collections::HashMap;
+use std::fs::File;
+use std::io::BufReader;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
@@ -14,6 +17,7 @@ extern crate rocket;
 
 const NUM_SENSORS: usize = 12;
 const CS_PINS: [u8; NUM_SENSORS] = [14, 4, 15, 18, 27, 23, 20, 5, 1, 7, 25, 24];
+const CALIBRATION_FILE: &str = "calibration.json";
 
 struct Temperatures {
     pub inner: HashMap<usize, f64>,
@@ -32,6 +36,16 @@ impl Temperatures {
             inner: HashMap::new(),
             calibration: default_calibration,
         }
+    }
+
+    pub fn load_calibration(&mut self, path: &str) -> Result<()> {
+        let calibration_file =
+            File::open(CALIBRATION_FILE).context("Failed to read calibration file")?;
+        let reader = BufReader::new(calibration_file);
+
+        self.calibration =
+            serde_json::from_reader(reader).context("Failed to parse calibration file")?;
+        Ok(())
     }
 }
 
@@ -86,7 +100,11 @@ async fn metrics(temperatures: &State<Arc<Mutex<Temperatures>>>) -> String {
 
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
-    let temperatures = Arc::new(Mutex::new(Temperatures::new(NUM_SENSORS)));
+    let mut temperatures = Temperatures::new(NUM_SENSORS);
+    temperatures
+        .load_calibration(CALIBRATION_FILE)
+        .expect("Failed to load calibration");
+    let temperatures = Arc::new(Mutex::new(temperatures));
 
     update_temp_periodically(temperatures.clone());
 
