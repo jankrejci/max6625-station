a399fba8f9220cd65a280f535c52ca380ff8d866
Jan Krejčí <krejci.cze@gmail.com>
2023-07-12 20:16:36 +0200

    FIXUP Introduce ds18b20 module

--- src/ds18b20.rs
+++ src/ds18b20.rs
@@ -18,7 +18,6 @@ pub struct Descriptor {
 impl DS18B20 {
     const BASE_PATH: &str = "/sys/bus/w1/devices/";
     const TARGET: &str = "w1_slave";
-    const UPDATE_PERIOD_MS: Duration = Duration::from_millis(400);
 
     pub fn open(id: &str) -> Self {
         let path = Path::new(Self::BASE_PATH).join(id).join(Self::TARGET);
@@ -30,25 +29,27 @@ impl DS18B20 {
     pub fn read_temp(&mut self) -> Result<f64> {
         let mut buffer = String::new();
         self.file.read_to_string(&mut buffer)?;
-        let lines = buffer.split("\n");
+        let lines = buffer.split('\n');
         Ok(0.0)
     }
+}
 
-    pub async fn update_temp_periodically(
-        descriptor: Descriptor,
-        ambient_temperature: Arc<Mutex<Option<f64>>>,
-    ) {
-        let mut temp = Self::open(&descriptor.id);
+pub async fn update_temp_periodically(
+    descriptor: Descriptor,
+    ambient_temperature: Arc<Mutex<Option<f64>>>,
+) {
+    const UPDATE_PERIOD_MS: Duration = Duration::from_millis(400);
 
-        loop {
-            let temperature_reading = temp.read_temp().ok();
-            {
-                let mut temperature = ambient_temperature
-                    .lock()
-                    .expect("BUG: Failed to acquire ambient_temperature lock");
-                *temperature = temperature_reading;
-            }
-            sleep(Self::UPDATE_PERIOD_MS).await;
+    let mut temp = DS18B20::open(&descriptor.id);
+
+    loop {
+        let temperature_reading = temp.read_temp().ok();
+        {
+            let mut temperature = ambient_temperature
+                .lock()
+                .expect("BUG: Failed to acquire ambient_temperature lock");
+            *temperature = temperature_reading;
         }
+        sleep(UPDATE_PERIOD_MS).await;
     }
 }
